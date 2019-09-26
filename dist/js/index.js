348da2cf7598e82df0f132af4f64afcce2afc232
Hong Baojin <13809735212@163.com>
2019-09-26 18:34:38 +0800

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  paginationClickable: true,
+  autoplay: 5000 //可选选项，自动滑动
+});
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+var mySwiper = new Swiper('.swiper-container', {
+  pagination : '.swiper-pagination',
+  paginationClickable :true,
+  autoplay: 5000,//可选选项，自动滑动
+});
